a6c28f75d49665cd962b8a7a551e574b0e11ac91
Bora Yüret <borayuret@gmail.com>
2025-04-15 13:40:46 +0300

    Add fx-api Spring Boot module

---
+++ fx-api/src/main/java/com/borayuret/fxapi/FxApiApplication.java
@@ -0,0 +1,13 @@
+package com.borayuret.fxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxApiApplication.class, args);
+    }
+
+}
